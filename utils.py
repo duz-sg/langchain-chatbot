4a633527dff6b8efc9d9bc1d82625f50fcbd0fa3
Du Zheng <deandz@gmail.com>
2024-03-14 17:54:38 -0400

    get google calendar items

--- utils.py
+++ utils.py
@@ -3,7 +3,15 @@ import random
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.agents import Tool
-from datetime import datetime
+import datetime as dt
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
 
 #decorator
 def enable_chat_history(func):
@@ -70,6 +78,78 @@ life_tool = Tool(
     )
 )
 
+def get_date_time(input=""):
+    now = dt.datetime.now()
+    local_now = now.astimezone()
+    local_tz = local_now.tzinfo
+    local_tzname = local_tz.tzname(local_now)
+    result = f"The date and time of today is {now}, {now.strftime('%A')}, {local_now}, {local_tz}, {local_tzname}"
+    print(result)
+    return result
+
+get_date_time_tool = Tool(
+    name="Get-current-date-and-time",
+    func=get_date_time,
+    description = (
+        "Useful when you need to answer question about current date time information."
+        # "Should only be used to answer question about meaning of life."
+    )
+)
+
+def auth_google():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return creds
+
+def get_google_events(input=""):
+    print(input)
+    try:
+        service = build("calendar", "v3", credentials=auth_google())
+        # Call the Calendar API
+        now = dt.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=10,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+        if not events:
+            print("No upcoming events found.")
+            return
+
+        results = []
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+            results.append(", ".join([start, event["summary"]]))
+        return results
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
 def get_visits(input=""):
     visits = ""
     with open("visits.txt", "r") as f:
@@ -78,7 +158,7 @@ def get_visits(input=""):
 
 get_visits_tool = Tool(
     name="Get-current-visits",
-    func=get_visits,
+    func=get_google_events,
     description = (
         "Useful when you need to answer question about user's visit plans for the future."
     )
@@ -98,23 +178,5 @@ set_visits_tool = Tool(
         "Useful when user wants to schedule a new event"
         "Input is the event in the format as below:"
         "{YYYY-MM-DD, Start time - Finish time, Evnet title}"
-    )
-)
-
-def get_date_time(input=""):
-    now = datetime.now()
-    local_now = now.astimezone()
-    local_tz = local_now.tzinfo
-    local_tzname = local_tz.tzname(local_now)
-    result = f"The date and time of today is {now}, {now.strftime('%A')}, {local_now}, {local_tz}, {local_tzname}"
-    print(result)
-    return result
-
-get_date_time_tool = Tool(
-    name="Get-current-date-and-time",
-    func=get_date_time,
-    description = (
-        "Useful when you need to answer question about current date time information."
-        # "Should only be used to answer question about meaning of life."
     )
 )
